5409cd11dc51fbb9a79b9f4f6243c06544c80210
Charlie Sheppard <sheppard.charlie24@gmail.com>
2022-02-07 01:25:14 -0800

    Complete back end

--- app.js
+++ app.js
@@ -1,9 +1,37 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const db = require("./config/keys").mongoURI;
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const User = require("./models/User");
+const bodyParser = require("body-parser");
+const passport = require("passport");
 
-// mongodb+srv://dev:cK76YsPZzOcm9egN@mern.31vi2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+mongoose.connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("This is working!"));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+app.use(bodyParser.json());
+
+// app.get("/", (req, res) => {
+//   const user = new User({
+//     handle: "Jim",
+//     email: "jim@jim.com",
+//     password: "jimisgreat"
+//   });
+//   user.save();
+//   res.send("This is working!");
+// });
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
 
 const port = process.env.PORT || 5000;
 
